17a9c47276de3251d4eaf361ad3edeb6c7f55003
Aftab Ahmad Khan <computer08.nls@gmail.com>
2025-08-06 01:06:46 +0500

    API

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+      "/api": {
+        target: "https://nls-portal-psi.vercel.app/",
+        changeOrigin: true,
       },
-      '/ws': {
-        target: 'ws://localhost:5000',
-        ws: true
-      }
-    }
+      "/ws": {
+        target: "ws://localhost:5000",
+        ws: true,
+      },
+    },
   },
   optimizeDeps: {
-    exclude: ['lucide-react']
-  }
+    exclude: ["lucide-react"],
+  },
 });
